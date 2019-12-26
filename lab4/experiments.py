acc5a09b9743f7f928517e90625674b5ef43990b
Edvard-Hagerup-Grieg <41989243+Edvard-Hagerup-Grieg@users.noreply.github.com>
2019-12-26 15:32:10 +0300

    Add files via upload

--- lab4/experiments.py
+++ lab4/experiments.py
@@ -1,4 +1,3 @@
-from lab2.dataset import load_dataset
 import keras.callbacks as K
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,7 +5,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 from lab2.dataset import load_dataset
-from lab4.models import generate_autoencoders_zoo, build_merged_models
+from lab4.models import generate_model_zoo_unsupervised
 
 
 def save_history_img(history, number, name='model'):
@@ -29,15 +28,6 @@ def save_history_img_enc(history, number, name='model'):
     plt.legend(['Train', 'Test'], loc='upper left')
     plt.savefig("pictures\\" + name + "_" + str(number))
     plt.clf()
-
-
-def calculate_accuracy(model, x, y):
-    y_prediction = np.argmax(model.predict(x), axis=1)
-    y_labels = np.argmax(y, axis=1)
-
-    val_accuracy = accuracy_score(y_labels, y_prediction)
-
-    return val_accuracy
 
 
 def train_models(models, x, y, name='model'):
@@ -76,7 +66,7 @@ def train_unsupervised_models(models, x, y):
     x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.25, random_state=42)
 
     for i, auroencoder in enumerate(models):
-        model = auroencoder[-1]
+        model = auroencoder
         model.compile(loss='mean_squared_error', optimizer='SGD', metrics=["mean_squared_error"])
 
         earlyStopping = K.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='min')
@@ -90,31 +80,10 @@ def train_unsupervised_models(models, x, y):
         model.load_weights(best_weights_filepath)
 
 
-def train_merged_models(models, x, y):
-    best_weights_filepath = './best_weights.hdf5'
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.25, random_state=42)
-
-    for i, auroencoder in enumerate(models):
-        model = auroencoder[-1]
-        model.compile(loss='mean_squared_error', optimizer='SGD', metrics=["mean_squared_error"])
-
-        earlyStopping = K.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='min')
-        mcp_save = K.ModelCheckpoint(best_weights_filepath, monitor='val_loss', verbose=1, save_best_only=True,
-                                     mode='auto')
-        history = model.fit(x, y, batch_size=2000, validation_data=(x_val, y_val), epochs=150,
-                            callbacks=[earlyStopping, mcp_save], verbose=1)
-
-        save_history_img(history, i)
-
-        model.load_weights(best_weights_filepath)
-
-
 if __name__ == "__main__":
-    autoencoders, ae_params = generate_autoencoders_zoo()
+    autoencoders, models, params = generate_model_zoo_unsupervised()
     (x_train, y_train), (x_test, y_test) = load_dataset()
     train_unsupervised_models(autoencoders, x_train, x_train)
 
-    for autoencoder, ae_param in zip(autoencoders, ae_params):
-        models, params = build_merged_models(autoencoder[0], autoencoder[1], ae_param[1][-1])
-        train_models(models, x_train, y_train, "new_moldel" + str(ae_param[0]))
-        test_models(models, x_test, y_test, "new_model" + str(ae_param[0]))
+    train_models(models, x_train, y_train, "new_moldel")
+    test_models(models, x_test, y_test, "new_model")
--- lab4/models.py
+++ lab4/models.py
@@ -3,39 +3,8 @@ from keras.models import Model
 import numpy as np
 
 
-def build_dense_model_1(input_size=784, number_of_classes=10, number_of_layers=1,
-                        hidden_sizes=[512], activation='relu'):
-    input_image = Input(shape=(input_size,))
-    x = input_image
-
-    for layer_num in range(number_of_layers):
-        x = Dense(hidden_sizes[layer_num], activation=activation)(x)
-
-    output = Dense(number_of_classes, activation="softmax")(x)
-
-    model = Model(input_image, output)
-    return model
-
-
 def build_simple_autoencoder(input_size=784, number_of_layers=1,
                              hidden_sizes=[512], activation='relu'):
-    input_image = Input(shape=(input_size,))
-    x = input_image
-
-    for layer_num in range(number_of_layers):
-        x = Dense(hidden_sizes[layer_num], activation=activation)(x)
-
-    for layer_num in range(number_of_layers):
-        x = Dense(hidden_sizes[-1 - layer_num], activation=activation)(x)
-
-    output = Dense(input_size, activation=activation)(x)
-
-    model = Model(input_image, output)
-    return model
-
-
-def build_simple_encoder(input_size=784, number_of_layers=1,
-                         hidden_sizes=[512], activation='relu'):
     input_image = Input(shape=(input_size,))
     x = input_image
 
@@ -59,48 +28,28 @@ def build_simple_encoder(input_size=784, number_of_layers=1,
     return input_image, encoder, decoder, autoencoder
 
 
-def generate_autoencoders_zoo():
+def generate_model_zoo_unsupervised():
     models = []
-    parameters_list = [[1, [512], 'relu'],
-                       [2, [512, 256], 'relu'],
+    autoencoders = []
+    parameters_list = [[2, [512, 256], 'relu'],
+                       [3, [512, 256, 64], 'linear'],
+                       [2, [512, 256], 'linear'],
                        [3, [512, 256, 64], 'relu']]
 
     for parameters in parameters_list:
-        models.append(build_simple_encoder(
-            number_of_layers=parameters[0], hidden_sizes=parameters[1], activation=parameters[2]
-        ))
+        input_image, encoder, decoder, autoencoder = build_simple_autoencoder(number_of_layers=parameters[0],
+                                                                              hidden_sizes=parameters[1],
+                                                                              activation=parameters[2])
 
-    return models, parameters_list
+        model_input = Input(shape=(parameters[1][-1],))
+        x = Dense(10, activation="softmax")(model_input)
+        model = Model(model_input, x)
+        classifier = Model(input_image, model(encoder(input_image)))
+        models.append(classifier)
+        autoencoders.append(autoencoder)
 
-
-def generate_model_zoo(input_size=512):
-    models = []
-    parameters_list = [[1, [input_size // 2], 'relu'],
-                       [2, [input_size // 2, input_size // 4], 'relu'],
-                       [3, [input_size // 2, input_size // 4, input_size // 8], 'relu']]
-
-    for parameters in parameters_list:
-        models.append(
-            build_dense_model_1(input_size=input_size, number_of_layers=parameters[0], hidden_sizes=parameters[1],
-                                activation=parameters[2]))
-
-    return models, parameters_list
-
-
-def build_merged_models(input_image, encoder, input_size=512):
-    models = []
-    parameters_list = [[1, [input_size // 2], 'relu'],
-                       [2, [input_size // 2, input_size // 4], 'relu'],
-                       [3, [input_size // 2, input_size // 4, input_size // 8], 'relu']]
-
-    for parameters in parameters_list:
-        model = build_dense_model_1(input_size=input_size, number_of_layers=parameters[0], hidden_sizes=parameters[1],
-                                    activation=parameters[2])
-
-        new_model = Model(input_image, model(encoder(input_image)))
-        models.append(new_model)
-    return models, parameters_list
+    return autoencoders, models, parameters_list
 
 
 if __name__ == "__main__":
-    models, parameters = generate_model_zoo()
+    models, parameters = generate_model_zoo_unsupervised()
